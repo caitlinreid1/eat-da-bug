352aee674f280bd5bdbbd07b1e9bfab095bf67a8
C. Reid <ctlnrd@gmail.com>
2017-05-16 15:08:33 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -3,28 +3,29 @@ var mysql = require("mysql");
 
 var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bugs_db" //////
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "bugs_db" //////
+// });
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
+    port: 3306,
     host: 'localhost',
     user: 'root',
     password: '',
