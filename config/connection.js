5e0a52d182b27253f407d2da852b68cb64b2ce41
C. Reid <ctlnrd@gmail.com>
2017-05-16 14:29:59 -0400

    fixed package.json

--- config/connection.js
+++ config/connection.js
@@ -11,14 +11,14 @@ var connection;
 //   database: "bugs_db" //////
 // });
 
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 
 if (process.env.JAWSDB_URL) {
